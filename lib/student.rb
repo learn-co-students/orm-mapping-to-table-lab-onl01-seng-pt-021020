d52b2167dd833cac3e01b0d9892e722ff6e615fb
Amber Williams <awilliams1234@live.com>
2020-04-13 20:42:11 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -33,12 +33,10 @@ class Student
 
   def self.create(name:, grade:)
     student = Student.new(name, grade)
-    student
     student.save
+    student
   end
-
+end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
-end
