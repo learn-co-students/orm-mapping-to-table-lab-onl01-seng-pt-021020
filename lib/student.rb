a33d531947c774efe8e27cf02d51ccbf73d5ab9d
Amber Williams <awilliams1234@live.com>
2020-04-13 20:35:54 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -31,6 +31,13 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def self.create(hash)
+    Student.all.each do |hash|
+      Student.new
+    save
+
+  end
+
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
